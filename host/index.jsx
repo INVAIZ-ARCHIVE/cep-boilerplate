6330b8634f05a8ad7efe1294586ae9f5af247526
ChangjunPark <ckdwns9121@naver.com>
2022-11-15 13:31:19 +0900

    # CEP Node server boilerplate

--- host/index.jsx
+++ host/index.jsx
@@ -1,36 +1,19 @@
 function evalFiles(jsxFolderPath) {
   var folder = new Folder(jsxFolderPath);
-  alert("load");
   if (folder.exists) {
     var jsxFiles = folder.getFiles("*.jsx");
     for (var i = 0; i < jsxFiles.length; i++) {
       var jsxFile = jsxFiles[i];
-      alert("i");
       try {
-        alert(jsxFile.toString());
+        $.evalFile(jsxFile);
       } catch (e) {
-        alert(e.toString());
         return "Exception " + e;
       }
     }
   }
 }
-function openDocument() {
-  const obj = {
-    name: "hello",
-  };
-  const APP = app;
-  debugger;
-  log(APP);
-}
 
-function test() {
-  alert("test start");
-  test.run();
+function openDocument(location) {
+  var fileRef = new File(location);
+  var docRef = app.open(fileRef);
 }
-function log(msg) {
-  $.writeln(msg);
-}
-
-$.bp();
-alert("hello");
