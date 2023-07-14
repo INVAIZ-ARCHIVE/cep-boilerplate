9ef08960eea23af177edd9fbd12c74467c5890bc
ChangjunPark <ckdwns9121@naver.com>
2023-07-14 11:17:20 +0900

    # cep 개발환경 수정

--- save.bat
+++ save.bat
@@ -1,4 +1,5 @@
-xcopy /E jsxbin %userprofile%\AppData\Roaming\Adobe\CEP\extensions\test\jsxbin
-xcopy /E host %userprofile%\AppData\Roaming\Adobe\CEP\extensions\test\host
-xcopy /E CSXS %userprofile%\AppData\Roaming\Adobe\CEP\extensions\test\CSXS
-xcopy /E client %userprofile%\AppData\Roaming\Adobe\CEP\extensions\test\client
+xcopy /E jsxbin %userprofile%\AppData\Roaming\Adobe\CEP\extensions\invaiz-test\jsxbin
+xcopy /E host %userprofile%\AppData\Roaming\Adobe\CEP\extensions\invaiz-test\host
+xcopy /E CSXS %userprofile%\AppData\Roaming\Adobe\CEP\extensions\invaiz-test\CSXS
+xcopy /E client %userprofile%\AppData\Roaming\Adobe\CEP\extensions\invaiz-test\client
+copy ".debug" %userProfile%\AppData\Roaming\Adobe\CEP\extensions\invaiz-test
